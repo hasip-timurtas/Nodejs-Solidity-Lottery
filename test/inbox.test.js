fa5d40761ab05e5f980f489958403b8991e065ea
Hasip Timurtas <hasip.timurtas@gmail.com>
2024-07-30 11:25:50 +0200

    example test casde added

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,5 +1,19 @@
 const assert = require('assert');
 const ganache = require('ganache');
-const Web3 = require('web3');
+const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(1);
+    });
+});
