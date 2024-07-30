ac4e7b74ca81d75c763be2b9dc3edb20c2b18a93
Hasip Timurtas <hasip.timurtas@gmail.com>
2024-07-30 15:44:42 +0200

    deploy contract implemented

--- deploy.js
+++ deploy.js
@@ -3,4 +3,20 @@ const { Web3 } = require('web3');
 const { interface, bytecode } = require('./compile');
 require('dotenv').config();
 
-const provider = new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_URL);
+const provider = new HDWalletProvider(process.env.WALLET_MNEMONIC, process.env.INFURA_URL);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    // using my second account to deploy the contract
+    console.log('Attempting to deploy from account', accounts[1]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+}
+
+deploy();
