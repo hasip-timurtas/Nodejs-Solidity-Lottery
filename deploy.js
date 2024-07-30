d76bcbfbbfde7be3c7199cfb241bd16290af7126
Hasip Timurtas <hasip.timurtas@gmail.com>
2024-07-30 15:46:53 +0200

    deployment reverted to main account

--- deploy.js
+++ deploy.js
@@ -8,8 +8,7 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    // using my second account to deploy the contract
-    console.log('Attempting to deploy from account', accounts[1]);
+    console.log('Attempting to deploy from account', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
