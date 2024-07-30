bb20e416b25daf8f7f241f215391498003b95e72
Hasip Timurtas <hasip.timurtas@gmail.com>
2024-07-30 11:33:00 +0200

    Deploy contract test case implemented

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,18 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-    console.log(accounts);
     // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(1);
+        console.log(inbox);
     });
 });
